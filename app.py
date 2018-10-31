4e85e23d580ff1b42ec80c1387ee452d4b418b24
a.zubarev <a.zubarev@mac410rih.rambler.ramblermedia.com>
2018-10-31 11:24:07 +0300

    recv

---
+++ app.py
@@ -0,0 +1,36 @@
+from tkinter import *
+import socket
+
+
+def pushbutton():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = 'ya.ru'
+    port = 80
+    s.connect((host, port))
+    s.send(b'GET / HTTP/1.1\r\n\r\n')
+    data = s.recv(1024)
+    print(data)
+    s.close()
+
+
+root = Tk()
+root.title("GUI на Python")
+root.geometry("300x250")
+
+message = StringVar()
+message_entryentr = Label(text="ведите адресс:")
+message_entryexit = Label(text="вывод данных:")
+
+message_entryentr.grid(row=1, column=1, padx=5, pady=5)
+message_entryexit.grid(row=10, column=2, padx=5, pady=5)
+
+message_entryentr = Entry(textvariable=message)
+message_entryentr.place(relx=.6, rely=.1, anchor="c")
+
+message_entryexit = Entry()
+message_entryexit.place(relx=.5, rely=.5, anchor="c", height=100, width=200, )
+
+btn = Button(text="скачать", command=pushbutton)
+btn.place(relx=.5, rely=.85, anchor="c", height=30, width=130, bordermode=OUTSIDE)
+
+root.mainloop()
